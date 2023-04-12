5442c8e84d146953f3a9a03c56e8a8c73f7fa62b
Moisés Morillo <kemdmq@gmail.com>
2023-04-12 11:21:40 -0500

    feat: added instructions to install packer

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Clean packer
+rm -rf ~/.local/share/nvim/site/pack/packer
+rm ~/.config/nvim/plugin/packer_compiled.lua
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+
 # Dectect OS
 if [[ "$(uname)" == "Darwin" ]]; then
   echo "MacOS"
