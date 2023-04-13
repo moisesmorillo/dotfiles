5bfa42bc79d035d72038b0732cfc108672e955fc
Moisés Morillo <kemdmq@gmail.com>
2023-04-13 08:45:37 -0500

    feat: added compatibility for codespaces

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
-. ./scripts/utils.sh
-
-os_name=$(get_os_name)
-
-if [[ "$os_name" == "debian" || "$os_name" == "ubuntu" ]]; then
-    # Install Debian/Ubuntu Packages
-    . ./scripts/install-debian.sh
-elif [[ "$os_name" == "fedora" ]]; then
-    # Install Fedora Packages
-    . ./scripts/install-fedora.sh
-elif [[ "$os_name" == "macos" ]]; then
-    # Install Mac Packages
-    . ./scripts/install-mac.sh
+if [ -n "$CODESPACES" ]; then
+  echo "***Loading Codespaces Dotfiles***"
+  chmod +x ./scripts/install-codespaces.sh
+  . ./scripts/install-codespaces.sh
+else
+    . ./scripts/utils.sh
+    os_name=$(get_os_name)
+    if [[ "$os_name" == "fedora" ]]; then
+        # Install Fedora Packages
+        . ./scripts/install-fedora.sh
+    elif [[ "$os_name" == "macos" ]]; then
+        # Install Mac Packages
+        . ./scripts/install-mac.sh
+    fi
 fi
