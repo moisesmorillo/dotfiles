df0e906e53ed51e7d8630a0fdc63b13a58558e04
Moisés Morillo <kemdmq@gmail.com>
2023-04-22 13:15:01 -0500

    feat: refactor lua config

--- .config/nvim/after/plugin/langs/lua.lua
+++ .config/nvim/after/plugin/langs/lua.lua
@@ -2,30 +2,37 @@ local ok, lspconfig = pcall(require, "lspconfig")
 local _, lsp = pcall(require, "lsp-zero")
 
 if ok then
-  lspconfig.lua_ls.setup(
-    lsp.nvim_lua_ls({
-      settings = {
-        Lua = {
-          completion = {
-            autoRequire = true,
-            enable = true,
-          },
-          hint = {
-            enable = true,
-            paramName = "all",
-          },
-          workspace = {
-            library = {
-              -- Make the server aware of Neovim runtime files
-              vim.fn.expand("$VIMRUNTIME/lua"),
-              vim.fn.stdpath("config") .. "/lua",
-              vim.api.nvim_get_runtime_file("", true),
-            }
-          }
+  local config = {
+    settings = {
+      Lua = {
+        codeLens = {
+          enable = true,
         },
+        completion = {
+          autoRequire = true,
+          enable = true,
+          callSnippet = "Both",
+          displayContext = true,
+          showParams = true,
+        },
+        diagnostics = {
+          enable = true,
+          workspaceDelay = 500,
+        },
+        hint = {
+          enable = true,
+          paramName = "all",
+        },
+        workspace = {
+          library = {
+            vim.fn.expand("$VIMRUNTIME/lua"),
+            vim.fn.stdpath("config") .. "/lua/core",
+          }
+        }
       },
-    })
-  )
+    },
+  }
+  lspconfig.lua_ls.setup(lsp.nvim_lua_ls(config))
 else
   print("Could not configure lua language support")
 end
