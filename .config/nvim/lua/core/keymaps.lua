7f0a5e2bbbb76f7bd3508425a9c84d25db52c33c
Moisés Morillo <kemdmq@gmail.com>
2023-04-24 09:23:14 -0500

    feat: added new keymaps to work with buffers

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -8,11 +8,21 @@ utils.nnoremap("<leader>w", ":w<cr>")
 -- Quit
 utils.nnoremap("<leader>q", ":q<cr>")
 
+-- Close Buffer
+utils.nnoremap("<leader>bd", ":bd<cr>")
+-- Next Buffer
+utils.nnoremap("<leader>bn", ":bn<cr>")
+-- Previous Buffer (before)
+utils.nnoremap("<leader>bb", ":bp<cr>")
+
 -- Turn off highlighting until next search using ctr+l
 utils.nnoremap("<c-l>", ":noh<cr>")
 
 -- Open/Quit NvimTree
 utils.nnoremap("<s-tab>", ":NvimTreeToggle<cr>")
+
+-- Open new buffer
+utils.nnoremap("<leader>n", ":enew<cr>")
 
 -- Telescope
 local ok, _ = pcall(require, "telescope.builtin")
@@ -35,6 +45,6 @@ vim.cmd [[
   autocmd FileType go nnoremap <leader>gtf <plug>(go-test-func)
   autocmd FileType go nnoremap <leader>gc <plug>(go-coverage-toggle)
   autocmd FileType go nnoremap <c-n> :cnext<cr>
-  autocmd FileType go nnoremap <c-p> :cprevious<cr> 
-  autocmd FileType go nnoremap <c-a> :cclose<cr> 
+  autocmd FileType go nnoremap <c-p> :cprevious<cr>
+  autocmd FileType go nnoremap <c-a> :cclose<cr>
 ]]
