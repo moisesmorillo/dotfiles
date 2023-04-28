f22b3b581bba6f410d23985bba56aa861672e0b1
Moisés Morillo <kemdmq@gmail.com>
2023-04-27 21:53:13 -0500

    feat: added copilot

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -48,3 +48,8 @@ vim.cmd [[
   autocmd FileType go nnoremap <c-p> :cprevious<cr>
   autocmd FileType go nnoremap <c-a> :cclose<cr>
 ]]
+
+-- Github Copitlot mappings
+vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-H>", 'copilot#Previous()', { silent = true, expr = true })
+vim.api.nvim_set_keymap("i", "<C-K>", 'copilot#Next()', { silent = true, expr = true })
