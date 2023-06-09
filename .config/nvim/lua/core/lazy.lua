6c768bc3ecdba742a986accc2ec3c50f2b61d1f9
Moisés Morillo <kemdmq@gmail.com>
2023-06-08 21:25:46 -0500

    fix: moved all config to separated files

--- .config/nvim/lua/core/lazy.lua
+++ .config/nvim/lua/core/lazy.lua
@@ -11,43 +11,10 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
---TODO finish this setting into separated files
-local plugins = {
-  -- Core
-  { "fatih/vim-go",                    build = ":GoInstallBinaries" },
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
-    dependencies = {
-      -- LSP Support
-      { "neovim/nvim-lspconfig" },
-      {
-        "williamboman/mason.nvim",
-        build = function()
-          _, _ = pcall(vim.cmd, "MasonUpdate")
-        end,
-      },
-      { "williamboman/mason-lspconfig.nvim" },
-
-      -- Autocompletion
-      { "hrsh7th/nvim-cmp" },
-      { "hrsh7th/cmp-buffer" },
-      { "hrsh7th/cmp-path" },
-      { "saadparwaiz1/cmp_luasnip" },
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "hrsh7th/cmp-nvim-lua" },
-
-      -- Snippets
-      { "L3MON4D3/LuaSnip" },
-      { "rafamadriz/friendly-snippets" },
-    },
-  },
-}
-
 local options = {
   defaults = {
     lazy = true,
-  }
+  },
 }
 
 require("lazy").setup("plugins", options)
