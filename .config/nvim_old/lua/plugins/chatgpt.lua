30acad71e57a8cf54f4a7df736480bd76b160501
Moises Morillo <kemdmq@gmail.com>
2023-06-23 09:19:47 -0500

    feat: backup old nvim config

--- .config/nvim_old/lua/plugins/chatgpt.lua
+++ .config/nvim_old/lua/plugins/chatgpt.lua
@@ -3,7 +3,10 @@ return {
   event = "VeryLazy",
   config = function()
     require("chatgpt").setup({
-      api_key_cmd = "op read op://Work/OpenAI_API_Key/api_key_cmd --no-newline"
+      api_key_cmd = "op read op://Work/OpenAI_API_Key/api_key_cmd --no-newline",
+      openai_params = {
+        model = "gpt-4",
+      },
     })
   end,
   dependencies = {
