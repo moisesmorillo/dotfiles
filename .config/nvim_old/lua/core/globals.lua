a11b9e26cc1ea3a8fe3d32af035c966bf4c1ab10
Moises Morillo <kemdmq@gmail.com>
2023-06-23 23:06:44 -0500

    chore: removed old plain neovim config

--- .config/nvim_old/lua/core/globals.lua
+++
@@ -1,9 +0,0 @@
-local globals = {
-  loaded_netrw = 1,
-  loaded_netrwPlugin = 1,
-  mapleader = " ",
-}
-
-for k, v in pairs(globals) do
-  vim.g[k] = v
-end
--- .config/nvim_old/lua/plugins/comment.lua
+++
@@ -1,9 +0,0 @@
-local keys = {
-    {"<leader>/", "<Plug>(comment_toggle_linewise_current)", desc = "Toggle comments in normal mode", silent = true, noremap = true},
-    {"<leader>/", "<Plug>(comment_toggle_blockwise_visual)", desc = "Toggle comments in visual mode", mode="v", silent = true, noremap = true},
-}
-
-return {
-    "numToStr/Comment.nvim",
-    keys = keys,
-}
