2215af7fb52b678d7e5fdb4f89f9daa598f6a3f2
Moisés Morillo <kemdmq@gmail.com>
2023-06-26 12:47:31 -0500

    fix: added dap_virtual_text

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -1,16 +1,23 @@
 local options = {
-  colorcolumn = "80",
-  modelines = 0,
-  number = true,
-  rnu = true,
-  ruler = true,
-  textwidth = 80,
-  scrolloff = 8,
-  showmatch = true,
-  swapfile = false,
+	colorcolumn = "80",
+	modelines = 0,
+	number = true,
+	rnu = true,
+	ruler = true,
+	textwidth = 80,
+	scrolloff = 8,
+	showmatch = true,
+	swapfile = false,
 }
 
 for k, v in pairs(options) do
-  vim.opt[k] = v
+	vim.opt[k] = v
 end
 
+local globals = {
+	dap_virtual_text = true,
+}
+
+for k, v in pairs(globals) do
+	vim.g[k] = v
+end
