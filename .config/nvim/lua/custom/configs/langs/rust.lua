74b46b97f67e05683a23157938582c33a165268e
Moisés Morillo <kemdmq@gmail.com>
2023-06-26 10:02:53 -0500

    feat: improved rust lsp support by adding root dir detector

--- .config/nvim/lua/custom/configs/langs/rust.lua
+++ .config/nvim/lua/custom/configs/langs/rust.lua
@@ -1,10 +1,12 @@
 local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
+local util = require("lspconfig/util")
 
 local options = {
 	on_attach = on_attach,
 	capabilities = capabilities,
 	filetypes = { "rust" },
+	root_dir = util.root_pattern("Cargo.toml", "Cargo.lock", ".git"),
 }
 
 return options
