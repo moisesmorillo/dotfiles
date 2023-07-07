72d4481ff3937499a214d7e4c3dbd76fb34a9eb8
Moises Morillo <kemdmq@gmail.com>
2023-07-07 07:41:17 -0500

    feat: added poetry env discovery

--- .config/nvim/lua/custom/configs/langs/python.lua
+++ .config/nvim/lua/custom/configs/langs/python.lua
@@ -3,9 +3,20 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local lspconfig = require("lspconfig")
 local util = require("lspconfig.util")
 
+local get_poetry_venv_path = function()
+	local fn = vim.fn
+	return fn.trim(fn.system("poetry config virtualenvs.path"))
+end
+
 lspconfig.pyright.setup({
 	on_attach = on_attach,
 	capabilities = capabilities,
 	filetypes = { "python" },
 	root_dir = util.root_pattern("requirements.txt", "pyproject.toml", "poetry.lock", ".git"),
+
+	settings = {
+		pyright = {
+			venvPath = get_poetry_venv_path(),
+		},
+	},
 })
