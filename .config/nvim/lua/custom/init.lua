d77aee21fc2a135aaa782c12722d8f1910cdc9a7
Moises Morillo <kemdmq@gmail.com>
2023-07-18 20:55:25 -0500

    feat: added Beam cursor for alacritty

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -30,3 +30,10 @@ local globals = {
 for k, v in pairs(globals) do
 	vim.g[k] = v
 end
+
+-- Fix cursor block when exiting from neovim
+vim.api.nvim_create_autocmd("VimLeave", {
+	callback = function()
+		vim.cmd([[set guicursor=a:ver100]])
+	end,
+})
