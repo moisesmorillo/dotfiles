8f85534a537fd08fe6065e7d24aa149a3c2f3edb
Moises Morillo <mmorillodev@pm.me>
2023-10-05 08:23:35 -0500

    fix: cmp mappings

--- nvim/.config/nvim/lua/custom/configs/langs/init.lua
+++ nvim/.config/nvim/lua/custom/configs/langs/init.lua
@@ -7,8 +7,30 @@ local plugins = {
       local cmp_conf = require "plugins.configs.cmp"
 
       cmp_conf.mapping = {
-        ["<C-k>"] = cmp.mapping.select_prev_item(),
-        ["<C-j>"] = cmp.mapping.select_next_item(),
+        ["<C-j>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif require("luasnip").expand_or_jumpable() then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
+          else
+            fallback()
+          end
+        end, {
+          "i",
+          "s",
+        }),
+        ["<C-k>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif require("luasnip").jumpable(-1) then
+            vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
+          else
+            fallback()
+          end
+        end, {
+          "i",
+          "s",
+        }),
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-e>"] = cmp.mapping.close(),
@@ -16,6 +38,7 @@ local plugins = {
           behavior = cmp.ConfirmBehavior.Insert,
           select = true,
         },
+        ["<C-space>"] = cmp.mapping.complete(),
       }
 
       return cmp_conf
