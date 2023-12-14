15c063ce687351642b94a53112373a1eb2afcb71
Moises Morillo <mmorillodev@pm.me>
2023-12-13 23:31:19 -0500

    feat: set neotree confg

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,11 +1,40 @@
+local get_buff_cwd = function()
+	local buffer_id = vim.api.nvim_get_current_buf()
+	local buffer_path = vim.api.nvim_buf_get_name(buffer_id)
+	local buffer_directory = vim.fn.fnamemodify(buffer_path, ":h:h")
+	return buffer_directory
+end
+
 return {
 	"nvim-neo-tree/neo-tree.nvim",
+	keys = {
+		{
+			"<leader>e",
+			function()
+				require("neo-tree.command").execute({ dir = vim.fn.getcwd(), reveal = true, toggle = true })
+			end,
+			desc = "Explorer NeoTree (root dir)",
+		},
+		{
+			"<leader>fe",
+			function()
+				require("neo-tree.command").execute({ dir = get_buff_cwd(), reveal = true, toggle = true })
+			end,
+			desc = "Explorer NeoTree (buffer cwd)",
+		},
+		{ "<leader>E", false },
+		{ "<leader>fE", false },
+		{ "<leader>be", false },
+		{ "<leader>ge", false },
+	},
 	opts = {
 		filesystem = {
-			hide_dotfiles = false,
-			hide_gitignored = false,
-			hide_by_name = {
-				"node_modules",
+			filtered_items = {
+				hide_by_name = {
+					"node_modules",
+				},
+				hide_dotfiles = false,
+				hide_gitignored = false,
 			},
 		},
 		window = {
