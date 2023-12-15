622bdf816aaafd1b31d55346d61017cd853d71d9
Moises Morillo <mmorillodev@pm.me>
2023-12-14 21:49:51 -0500

    feat: migrate options from nvchad

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,32 @@
+local opt = vim.opt
+local options = {
+	colorcolumn = "120",
+	modelines = 0,
+	number = true,
+	rnu = true,
+	ruler = true,
+	textwidth = 120,
+	scrolloff = 8,
+	showmatch = true,
+	swapfile = false,
+	fillchars = { eob = "~" },
+}
+
+-- better search
+opt.path:remove("/usr/include")
+opt.path:append("**")
+opt.wildignorecase = true
+opt.wildignore:append("**/node_modules/*")
+opt.wildignore:append("**/.git/*")
+
+for k, v in pairs(options) do
+	opt[k] = v
+end
+
+local globals = {
+	dap_virtual_text = true,
+}
+
+for k, v in pairs(globals) do
+	vim.g[k] = v
+end
