4295c5bc89e1c42cc7e6b4046f37273872867c31
Moises Morillo <mmorillodev@pm.me>
2023-12-17 20:42:03 -0500

    feat: enable dap core

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -16,6 +16,7 @@ require("lazy").setup({
 		-- add LazyVim and import its plugins
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
 		{ import = "lazyvim.plugins.extras.test.core" },
+		{ import = "lazyvim.plugins.extras.dap.core" },
 		{ import = "plugins" },
 		{ import = "plugins.extras.lang" },
 		{ import = "lazyvim.plugins.extras.lang.go" },
