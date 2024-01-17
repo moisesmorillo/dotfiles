2f8ff4ba87274dc9dd52b5ab9cf320fd9e096d21
Moises Morillo <mmorillodev@pm.me>
2024-01-16 20:11:49 -0500

    feat: added mapping to open dotfiles from telescope

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,11 @@ local map = vim.keymap.set
 -- LSP
 map("n", "<leader>cli", "<cmd>LspInfo<cr>", { desc = "LSP Info" })
 map("n", "<leader>cld", "<cmd>Neoconf lsp<cr>", { desc = "LSP Debug" })
+
+-- Telescope
+map("n", "<leader>fd", function()
+	require("telescope.builtin").git_files({
+		prompt_title = "<Dotfiles>",
+		cwd = "~/projects/dotfiles/",
+	})
+end, { desc = "Find Dotfiles" })
