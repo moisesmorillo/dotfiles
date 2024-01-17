59f9400b8397a63a601166d1bc6f52af46e00e1d
Moises Morillo <mmorillodev@pm.me>
2024-01-17 14:56:11 -0500

    fix: removed nvchad install script

--- scripts/clean-install-nvchad-distro.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-stow -D nvim -t $HOME
-rm -rf ~/.config/nvim/ ~/.local/share/nvim/ ~/.local/state/nvim/ ~/.cache/nvim/
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
-stow -R -t $HOME nvim
