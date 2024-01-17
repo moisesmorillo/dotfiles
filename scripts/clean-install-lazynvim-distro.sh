8d6f7c82b9489d3332c8f4ba487e8cf0a57d12ff
Moises Morillo <mmorillodev@pm.me>
2024-01-17 18:15:50 -0500

    fix: test new bash config

--- scripts/clean-install-lazynvim-distro.sh
+++ scripts/clean-install-lazynvim-distro.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-stow -D nvim -t $HOME
+stow -D nvim -t "$HOME"
 rm -rf ~/.config/nvim/ ~/.local/share/nvim/ ~/.local/state/nvim/ ~/.cache/nvim/
-stow -R -t $HOME nvim
+stow -R -t "$HOME" nvim
