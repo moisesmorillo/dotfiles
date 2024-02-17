6e842ed7ac6f51797df62a0f5052c9b9ca115ea1
Moises Morillo <mmorillodev@pm.me>
2024-02-16 20:31:27 -0500

    feat: migrate extra to lazyvim.json

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -15,44 +15,22 @@ require("lazy").setup({
 	spec = {
 		-- add LazyVim and import its plugins
 		{ "LazyVim/LazyVim", import = "lazyvim.plugins" },
-		-- dap and testing
-		{ import = "lazyvim.plugins.extras.dap.core" },
-		{ import = "lazyvim.plugins.extras.test.core" },
 		-- bash
 		{ import = "plugins.extras.lang.bash" },
-		-- c/c++
-		{ import = "lazyvim.plugins.extras.lang.clangd" },
+		-- c/cpp
 		{ import = "plugins.extras.lang.cpp" },
-		-- docker
-		{ import = "lazyvim.plugins.extras.lang.docker" },
-		-- python
-		{ import = "lazyvim.plugins.extras.lang.python" },
-		{ import = "plugins.extras.lang.python" },
 		-- go
-		{ import = "lazyvim.plugins.extras.lang.go" },
 		{ import = "plugins.extras.lang.go" },
 		-- lua
-		{ import = "lazyvim.plugins.extras.dap.nlua" },
 		{ import = "plugins.extras.lang.lua" },
-		-- markdown
-		{ import = "lazyvim.plugins.extras.lang.markdown" },
+		-- python
+		{ import = "plugins.extras.lang.python" },
+		-- rust
+		{ import = "plugins.extras.lang.rust" },
 		-- typescript
 		{ import = "plugins.extras.lang.typescript" },
-		-- rust
-		{ import = "lazyvim.plugins.extras.lang.rust" },
-		{ import = "plugins.extras.lang.rust" },
 		-- yaml
-		{ import = "lazyvim.plugins.extras.lang.yaml" },
 		{ import = "plugins.extras.lang.yaml" },
-		-- coding
-		{ import = "lazyvim.plugins.extras.coding.copilot" },
-		-- linting and formatting
-		{ import = "lazyvim.plugins.extras.linting.eslint" },
-		{ import = "lazyvim.plugins.extras.formatting.prettier" },
-		-- editor
-		{ import = "lazyvim.plugins.extras.editor.aerial" },
-		-- utils
-		{ import = "lazyvim.plugins.extras.util.project" },
 		-- local plugins
 		{ import = "plugins" },
 	},
