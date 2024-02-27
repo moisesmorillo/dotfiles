a9a0809f0d5b1571567e314f8a1f8a31e0ef1ad2
Moises Morillo <mmorillodev@pm.me>
2024-02-27 14:51:16 -0500

    feat: added linter and formatting

--- nvim/.config/nvim/lua/plugins/extras/lang/jenkins.lua
+++ nvim/.config/nvim/lua/plugins/extras/lang/jenkins.lua
@@ -6,4 +6,42 @@ return {
 			vim.list_extend(opts.ensure_installed, { "groovy" })
 		end,
 	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				groovyls = {},
+			},
+		},
+	},
+	{
+		"mfussenegger/nvim-lint",
+		opts = {
+			linters_by_ft = {
+				groovy = { "npm-groovy-lint" },
+			},
+			linters = {
+				["npm-groovy-lint"] = {
+					args = { "--failon", "error" },
+				},
+			},
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {
+			formatters_by_ft = {
+				groovy = { "npm_groovy_lint" },
+			},
+			formatters = {
+				npm_groovy_lint = {
+					command = "npm-groovy-lint",
+					args = { "--failon", "error", "--fix", "$FILENAME" },
+					cwd = require("conform.util").root_file({ ".git" }),
+					stdin = false,
+				},
+			},
+		},
+	},
 }
