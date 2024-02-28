a279955a3777345d6384bf763386f63b0458ae3a
Moises Morillo <mmorillodev@pm.me>
2024-02-27 21:00:35 -0500

    feat: improved jenkins support

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,6 +1,16 @@
+-- Start of Jenkinsfile (groovy) filetype --
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
 	pattern = "*.Jenkinsfile",
 	callback = function()
 		vim.bo.filetype = "groovy"
 	end,
 })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "groovy",
+	callback = function()
+		vim.opt_local.shiftwidth = 4
+		vim.opt_local.softtabstop = 4
+		vim.opt_local.expandtab = true
+	end,
+})
+-- End of Jenkinsfile (groovy) filetype --
