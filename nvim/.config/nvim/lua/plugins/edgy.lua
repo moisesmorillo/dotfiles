4dafffc7fd95514fd6b03628b997a2d54c8ed3db
Moises Morillo <mmorillodev@pm.me>
2024-03-05 22:14:19 -0500

    feat: improved neotest config in edgy

--- nvim/.config/nvim/lua/plugins/edgy.lua
+++ nvim/.config/nvim/lua/plugins/edgy.lua
@@ -2,8 +2,25 @@ return {
 	"folke/edgy.nvim",
 	opts = function(_, opts)
 		-- Swap the left and right sides, and leave the right side empty for NeoTree
-		opts.left = opts.right
+		opts.left = opts.right or {}
 		opts.right = {}
+
+		vim.list_extend(opts.left, {
+			{
+				title = "Neotest Summary",
+				ft = "neotest-summary",
+				size = { width = 0.4 },
+			},
+		})
+
+		vim.list_extend(opts.bottom, {
+			{
+				title = "Neotest Output",
+				ft = "neotest-output",
+			},
+		})
+
+		vim.print(vim.inspect(opts.bottom))
 
 		return opts
 	end,
