c84d3d2d938512c2515a88c19d67ea5840fa1fef
Moises Morillo <mmorillodev@pm.me>
2024-03-11 09:20:35 -0500

    fix: lazygit nvim mapping to read local conf

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,6 @@
 local map = require("lazyvim.util").safe_keymap_set
+local Util = require("lazyvim.util")
+local lazygitHomeDirConfig = os.getenv("HOME") .. "/.config/lazygit"
 
 -- LSP
 map("n", "<leader>cli", "<cmd>LspInfo<cr>", { desc = "LSP Info" })
@@ -18,3 +20,15 @@ map("t", "<c-h", "<cmd>wincmd h", { desc = "Go To Left Terminal", noremap = true
 map("t", "<c-j", "<cmd>wincmd j", { desc = "Go To Down Terminal", noremap = true, silent = true })
 map("t", "<c-k", "<cmd>wincmd k", { desc = "Go To Above Terminal", noremap = true, silent = true })
 map("t", "<c-l", "<cmd>wincmd l", { desc = "Go To Right Terminal", noremap = true, silent = true })
+
+-- Lazygit remap to use local config
+map("n", "<leader>gg", function()
+	Util.terminal(
+		{ "lazygit", "-ucd", lazygitHomeDirConfig },
+		{ cwd = lazygitHomeDirConfig, esc_esc = false, ctrl_hjkl = false },
+		{ cwd = Util.root(), esc_esc = false, ctrl_hjkl = false }
+	)
+end, { desc = "Lazygit (root dir)" })
+map("n", "<leader>gG", function()
+	Util.terminal({ "lazygit", "-ucd", lazygitHomeDirConfig }, { esc_esc = false, ctrl_hjkl = false })
+end, { desc = "Lazygit (cwd)" })
