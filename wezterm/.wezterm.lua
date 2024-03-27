ea19a7af6bc0022d6662d1eb606ced2376a3790b
Moises Morillo <mmorillodev@pm.me>
2024-03-27 15:46:33 -0500

    feat: simplify wezterm setup

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,10 +1,14 @@
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Mocha"
-config.window_background_opacity = 0.9
-
--- config.font = wezterm.font("JetBrainsMono", { weight = "Regular", style = "Regular" })
-config.font_size = 18
-
-return config
+return {
+	color_scheme = "Catppuccin Mocha",
+	font = wezterm.font_with_fallback({ "JetBrainsMono Nerd Font", "Fira Code" }),
+	font_size = 18,
+	window_background_opacity = 0.9,
+	window_padding = {
+		left = 0,
+		right = 0,
+		top = 0,
+		bottom = 0,
+	},
+}
