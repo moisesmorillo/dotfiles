d23feed4dd55fd37f95e58a556e98f4ba3a04bbb
Moises Morillo <mmorillodev@pm.me>
2024-03-27 16:33:17 -0500

    feat: added fps animations, audible_bell and cursor related configs

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,10 +1,17 @@
 local wezterm = require("wezterm")
 
 return {
+	animation_fps = 100,
+	audible_bell = "Disabled",
 	color_scheme = "Catppuccin Mocha",
+	cursor_blink_ease_in = "Constant",
+	cursor_blink_ease_out = "Constant",
+	cursor_blink_rate = 500,
+	cursor_thickness = 0.7,
+	default_cursor_style = "BlinkingBar",
 	font = wezterm.font_with_fallback({ "JetBrainsMono Nerd Font", "Fira Code" }),
 	font_size = 18,
-	window_background_opacity = 0.9,
+	window_background_opacity = 0.96,
 	window_padding = {
 		left = 0,
 		right = 0,
