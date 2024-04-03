89a81e3559185d8e256644ce53e927c8b11371d9
Moises Morillo <mmorillodev@pm.me>
2024-04-02 22:26:57 -0500

    fix: improved go.nvim settings

--- nvim/.config/nvim/lua/plugins/extras/lang/go.lua
+++ nvim/.config/nvim/lua/plugins/extras/lang/go.lua
@@ -50,10 +50,9 @@ return {
 	},
 	{
 		"ray-x/go.nvim",
+		ft = { "go", "gomod" },
 		dependencies = {
 			"ray-x/guihua.lua",
-			"neovim/nvim-lspconfig",
-			"nvim-treesitter/nvim-treesitter",
 		},
 		opts = {},
 		event = { "CmdlineEnter" },
