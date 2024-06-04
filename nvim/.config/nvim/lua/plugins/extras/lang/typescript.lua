40f86a79b678e4ffacb58fa7ef819c7b4dd72185
Moises Morillo <mmorillodev@pm.me>
2024-06-03 19:24:48 -0500

    feat: added vitst to neotest adapters

--- nvim/.config/nvim/lua/plugins/extras/lang/typescript.lua
+++ nvim/.config/nvim/lua/plugins/extras/lang/typescript.lua
@@ -3,12 +3,19 @@ return {
     "nvim-neotest/neotest",
     dependencies = {
       "nvim-neotest/neotest-jest",
+      "marilari88/neotest-vitest",
     },
     opts = {
       adapters = {
         ["neotest-jest"] = {
           jestCommand = "npx jest --coverage --ci",
           env = { NODE_OPTIONS = "--experimental-vm-modules" },
+        },
+        ["neotest-vitest"] = {
+          vitestCommand = "npx vitest --coverage --run",
+          filter_dir = function(name, _, _)
+            return name ~= "node_modules"
+          end,
         },
       },
     },
