0374483c9339733fe4d88173894d6df67de34e72
Moises Morillo <mmorillodev@pm.me>
2024-06-19 17:45:09 -0500

    feat: setup yamlfmt to use global conf if not local file is provided

--- nvim/.config/nvim/lua/plugins/extras/lang/yaml.lua
+++ nvim/.config/nvim/lua/plugins/extras/lang/yaml.lua
@@ -19,6 +19,18 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+      formatters = {
+        yamlfmt = {
+          args = function()
+            local local_config_file = vim.fn.getcwd() .. "/.yamlfmt"
+            if vim.fn.filereadable(local_config_file) == 1 then
+              return { "-conf", local_config_file, "$FILENAME" }
+            end
+
+            return { "-global_conf", "$FILENAME" }
+          end,
+        },
+      },
       formatters_by_ft = {
         yaml = { "yamlfmt" },
       },
