599f9ab8c7ff0fe5ed456a1033ac847805046ee8
Moises Morillo <mmorillodev@pm.me>
2024-06-27 12:29:24 -0500

    feat: improved copilotchat and neotree integration with edgy

--- nvim/.config/nvim/lua/plugins/edgy.lua
+++ nvim/.config/nvim/lua/plugins/edgy.lua
@@ -1,17 +1,40 @@
 return {
   "folke/edgy.nvim",
   opts = function(_, opts)
-    -- Swap the left and right sides, and leave the right side empty for NeoTree
-    opts.left = opts.right or {}
-    opts.right = {}
+    local left = opts.left or {}
+    local right = opts.right or {}
 
-    vim.list_extend(opts.left, {
+    for i, v in ipairs(right) do
+      if v.ft == "copilot-chat" then
+        vim.print("Removing Copilot Chat from the right side")
+        table.remove(right, i)
+        break
+      end
+    end
+
+    vim.list_extend(left, {
       {
-        title = "Neotest Summary",
-        ft = "neotest-summary",
-        size = { width = 0.4 },
+        ft = "copilot-chat",
+        title = "Copilot Chat",
+        pinned = true,
+        open = function()
+          vim.cmd("Neotree close")
+          vim.cmd("CopilotChat toggle")
+        end,
       },
     })
+
+    -- Swap the left and right sides, and leave the right side empty for NeoTree
+    opts.left = right
+    opts.right = left
+    opts.options = {
+      right = {
+        size = 50,
+      },
+    }
+    opts.animate = {
+      enabled = false,
+    }
 
     return opts
   end,
