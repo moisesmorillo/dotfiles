97ce970ac4ab7d7df48678f88a71a78e2c4b8967
Moises Morillo <mmorillodev@pm.me>
2024-06-27 10:31:10 -0500

    feat: better defaults for python

--- nvim/.config/nvim/lua/plugins/extras/lang/python.lua
+++ nvim/.config/nvim/lua/plugins/extras/lang/python.lua
@@ -1,5 +1,26 @@
 return {
   {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          settings = {
+            pyright = {
+              -- Using Ruff's import organizer
+              disableOrganizeImports = true,
+            },
+            python = {
+              analysis = {
+                -- Ignore all files for analysis to exclusively use Ruff for linting
+                ignore = { "*" },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
     "nvim-neotest/neotest",
     dependencies = {
       "nvim-neotest/neotest-python",
