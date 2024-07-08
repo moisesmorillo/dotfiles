0bc4292f6732549ee4e90ff48c61f1f383daf040
Moises Morillo <mmorillodev@pm.me>
2024-07-08 18:52:21 -0500

    feat: moved cmdline to center screen

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,6 +1,14 @@
 return {
   "folke/noice.nvim",
   opts = {
+    views = {
+      cmdline_popup = {
+        position = {
+          row = "50%",
+          col = "50%",
+        },
+      },
+    },
     presets = {
       lsp_doc_border = true,
     },
