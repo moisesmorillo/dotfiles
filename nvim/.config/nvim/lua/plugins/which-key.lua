86aa59fa0c4910fa7f99d4f761fc9221d3e5261a
Moises Morillo <mmorillodev@pm.me>
2024-07-08 20:07:46 -0500

    feat: removed unneeded keymaps from which-key

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -1,16 +1,10 @@
 return {
   "folke/which-key.nvim",
-  opts = function(_, opts)
-    opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
+  opts = {
+    defaults = {
       -- Coverage
       ["<leader>tc"] = { name = "+coverage" },
       -- LSP
-      ["<leader>cl"] = { name = "+lsp" },
-      -- Neogen
-      ["<leader>cg"] = { name = "+neogen" },
-      -- Overseer
-      ["<leader>o"] = { name = "+overseer" },
-    })
-    return opts
-  end,
+    },
+  },
 }
