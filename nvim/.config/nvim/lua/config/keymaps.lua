2f5ac16e706347768c96c32fdfb2cc9b78dbb736
Moises Morillo <mmorillodev@pm.me>
2024-12-19 09:14:52 -0500

    fix: keymaps after LazyVim 14 migration

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,6 +1,4 @@
-local map = require("lazyvim.util").safe_keymap_set
-local Util = require("lazyvim.util")
-local lazygitHomeDirConfig = os.getenv("HOME") .. "/.config/lazygit"
+local map = vim.keymap.set
 
 -- LSP
 map("n", "<leader>cli", "<cmd>LspInfo<cr>", { desc = "LSP Info" })
@@ -8,8 +6,8 @@ map("n", "<leader>cld", "<cmd>LazyDev lsp<cr>", { desc = "LSP Debug" })
 
 -- Telescope
 map("n", "<leader>fd", function()
-  require("telescope.builtin").git_files({
-    prompt_title = "<Dotfiles>",
+  require("fzf-lua").files({
+    prompt = "<Dotfiles> ",
     cwd = "~/projects/dotfiles/",
   })
 end, { desc = "Find Dotfiles" })
@@ -20,10 +18,3 @@ map("t", "<c-h", "<cmd>wincmd h", { desc = "Go To Left Terminal", noremap = true
 map("t", "<c-j", "<cmd>wincmd j", { desc = "Go To Down Terminal", noremap = true, silent = true })
 map("t", "<c-k", "<cmd>wincmd k", { desc = "Go To Above Terminal", noremap = true, silent = true })
 map("t", "<c-l", "<cmd>wincmd l", { desc = "Go To Right Terminal", noremap = true, silent = true })
-
--- Lazygit remap to use local config
-map("n", "<leader>gg", function()
-  Util.terminal({ "lazygit", "-ucd", lazygitHomeDirConfig }, { esc_esc = false, ctrl_hjkl = false })
-end, { desc = "Lazygit", noremap = true, silent = true })
-
-vim.keymap.del("n", "<leader>gG")
