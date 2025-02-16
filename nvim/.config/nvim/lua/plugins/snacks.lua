11b551fdc51c32da81a99908ea9a39511795f5be
Moisés Morillo <mmorillodev@pm.me>
2025-02-16 18:06:36 -0500

    feat: basic setup for snacks.nvim

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,21 @@
+---@type LazySpec
 return {
   "folke/snacks.nvim",
+  ---@type snacks.Config
   opts = {
+    ---@class snacks.explorer.Config
+    explorer = {
+      replace_netrw = true,
+    },
+    picker = {
+      sources = {
+        ---@class snacks.picker.explorer.Config
+        explorer = {
+          hidden = true,
+          layout = { layout = { position = "right" } },
+        },
+      },
+    },
     lazygit = {
       configure = true,
     },
