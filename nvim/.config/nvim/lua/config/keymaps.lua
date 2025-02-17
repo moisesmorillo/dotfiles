df28a84d136f0e43da5772398cae5f4f417008b7
Moisés Morillo <mmorillodev@pm.me>
2025-02-16 22:47:17 -0500

    fix: dotfiles finder

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -4,16 +4,16 @@ local map = vim.keymap.set
 map("n", "<leader>cli", "<cmd>LspInfo<cr>", { desc = "LSP Info" })
 map("n", "<leader>cld", "<cmd>LazyDev lsp<cr>", { desc = "LSP Debug" })
 
--- Telescope
+-- Picker
 map("n", "<leader>fd", function()
-  require("fzf-lua").files({
-    prompt = "<Dotfiles> ",
-    cwd = "~/projects/dotfiles/",
+  Snacks.picker.files({
+    hidden = true,
+    title = "<Dotfiles> ",
+    dirs = { "~/projects/dotfiles/" },
   })
 end, { desc = "Find Dotfiles" })
 
 -- ToggleTerm
-map("t", "<esc>", "<cmd>ToggleTerm<cr>", { desc = "Exit Terminal", noremap = true, silent = true })
 map("t", "<c-h", "<cmd>wincmd h", { desc = "Go To Left Terminal", noremap = true, silent = true })
 map("t", "<c-j", "<cmd>wincmd j", { desc = "Go To Down Terminal", noremap = true, silent = true })
 map("t", "<c-k", "<cmd>wincmd k", { desc = "Go To Above Terminal", noremap = true, silent = true })
