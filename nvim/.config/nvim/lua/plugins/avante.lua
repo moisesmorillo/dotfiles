085e9114be7d8ecdb46eebc59be93df003683dfe
Moisés Morillo <mmorillodev@pm.me>
2025-04-24 17:14:53 -0500

    feat: avante improvemets

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -2,45 +2,38 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
+  build = "make",
   dependencies = {
+    "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
+    "folke/snacks.nvim",
   },
-  version = "*",
+  version = false,
   opts = {
-    provider = "ollama",
-    vendors = {
-      ollama = {
-        __inherited_from = "openai",
-        api_key_name = "",
-        endpoint = "http://127.0.0.1:11434/v1",
-        model = "qwen2.5-coder",
-      },
+    provider = "gemini",
+    gemini = {
+      model = "gemini-2.5-flash-preview-04-17", --"gemini-2.5-pro-exp-03-25",
+    },
+    ollama = {
+      endpoint = "http://127.0.0.1:11434",
+      model = "qwen2.5-coder",
+      disable_tools = true,
     },
     file_selector = {
-      profider = "snacks",
+      provider = "snacks",
     },
-    providers = {
-      avante_commands = {
-        name = "avante_commands",
-        module = "blink.compat.source",
-        score_offset = 90, -- show at a higher priority than lsp
-        opts = {},
-      },
-      avante_files = {
-        name = "avante_files",
-        module = "blink.compat.source",
-        score_offset = 100, -- show at a higher priority than lsp
-        opts = {},
-      },
-      avante_mentions = {
-        name = "avante_mentions",
-        module = "blink.compat.source",
-        score_offset = 1000, -- show at a higher priority than lsp
-        opts = {},
-      },
+    selector = {
+      provider = "snacks",
+    },
+    rag_service = {
+      enabled = true,
+      provider = "ollama",
+      endpoint = "http://127.0.0.1:11434",
+      host_mount = "~/projects/",
+      llm_model = "qwen2.5-coder",
+      embed_model = "nomic-embed-text",
     },
   },
-  build = "make",
 }
