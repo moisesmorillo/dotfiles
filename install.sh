9eb33ebb5bdbc8129729bc346abb4ab8c9a82a14
Moisés Morillo <mmorillodev@pm.me>
2025-09-19 12:14:40 -0500

    chore: sync LazyVim plugins

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 if [ -n "$CODESPACES" ]; then
-  echo "***Loading Codespaces Dotfiles***"
-  chmod +x ./scripts/install-codespaces.sh
-  . ./scripts/install-codespaces.sh
+	echo "***Loading Codespaces Dotfiles***"
+	chmod +x ./scripts/install-codespaces.sh
+	. ./scripts/install-codespaces.sh
 else
-    . ./scripts/utils.sh
-    os_name=$(get_os_name)
-    if [[ "$os_name" == "macos" ]]; then
-      echo "***Loading MasOS Dotfiles***"
-      chmod +x ./scripts/install-mac.sh
-      . ./scripts/install-mac.sh
-    fi
+	. ./scripts/utils.sh
+	os_name=$(get_os_name)
+	if [[ "$os_name" == "macos" ]]; then
+		echo "***Loading MasOS Dotfiles***"
+		chmod +x ./scripts/install-mac.sh
+		. ./scripts/install-mac.sh
+	fi
 fi
